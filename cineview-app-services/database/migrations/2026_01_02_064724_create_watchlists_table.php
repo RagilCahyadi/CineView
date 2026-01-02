73f821d4fac1f7111d8e44152d7a39708f7c7eca
Ragil Cahyadi <ragilccahyadi@gmail.com>
2026-01-02 14:22:40 +0700

    Initialize setup project Laravel user, review, watchlist migrations, and Sanctum config.

---
+++ cineview-app-services/database/migrations/2026_01_02_064724_create_watchlists_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('watchlists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('movie_id');
+            $table->string('movie_title');
+            $table->string('poster_path')->nullable();
+            $table->timestamp('added_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('watchlists');
+    }
+};
