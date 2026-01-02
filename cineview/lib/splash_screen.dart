5390b2a793a35ea95345465bfa412b41f8b6315a
Muhammad Fakhrudin <muhfakhrudin04@gmail.com>
2026-01-02 21:43:00 +0700

    feat: implement splash screen and home page

---
+++ cineview/lib/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:cineview/home_pages.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage()));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFBCC1DA),
+      body: Center(
+        child: Padding(padding: const EdgeInsets.all(20.0),
+          child: Image.asset('assets/images/Logo_CineView.png',
+            width: 250,
+            fit: BoxFit.contain,
+          ),
+        ),
+      )
+    );
+  }
+}
