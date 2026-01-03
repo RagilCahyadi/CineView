4e6bfc04c9d915fd1a01885eefbaca9aaf9abc35
muhfakhrudin <muhfakhrudin04@gmail.com>
2026-01-03 12:38:23 +0700

    feat: implement splash screen and home page (#3)

--- cineview/lib/main.dart
+++ cineview/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cineview/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,14 +8,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyApp(),
+      home: const SplashScreen(),
     );
   }
 }
