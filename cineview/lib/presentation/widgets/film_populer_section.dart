e5e143955f8a79b0cc37963d2c1f2f251620b779
muhfakhrudin <muhfakhrudin04@gmail.com>
2026-01-05 02:32:28 +0700

    feat: Implement initial home page UI with featured, popular, and watchlist sections, movie cards, and dummy data. (#15)

---
+++ cineview/lib/presentation/widgets/film_populer_section.dart
@@ -0,0 +1,65 @@
+import 'package:cineview/presentation/widgets/movie_card.dart';
+import 'package:flutter/material.dart';
+import 'package:cineview/core/theme/app_theme.dart';
+import 'package:cineview/data/models/dummy_data_film.dart';
+
+class FilmPopulerSection extends StatelessWidget {
+  const FilmPopulerSection({super.key, required this.film});
+
+  final List<DummyDataFilm> film;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Film Populer",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: AppTheme.textPrimary,
+                ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    "See all",
+                    style: TextStyle(
+                      color: AppTheme.textSecondary,
+                      fontSize: 14,
+                    ),
+                  ),
+                  const SizedBox(width: 6),
+                  Icon(
+                    Icons.play_circle_filled,
+                    color: AppTheme.primaryColor,
+                    size: 20,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 16),
+
+        SizedBox(
+          height: 200,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            itemCount: film.length,
+            itemBuilder: (context, index) {
+              return MovieCard(film: film[index]);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
