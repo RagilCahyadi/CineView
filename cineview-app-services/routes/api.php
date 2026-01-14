a2661a53c660ceef207640e0e111399c473a5216
Ragil Cahyadi <ragilccahyadi@gmail.com>
2026-01-15 02:01:01 +0700

    feat: (API) add review CRUD routes, controller methods and adding new columns context to table reviews

--- cineview-app-services/routes/api.php
+++ cineview-app-services/routes/api.php
@@ -3,16 +3,28 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\WatchlistController;
+use App\Http\Controllers\Api\ReviewController;
 
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 
+Route::get('/reviews/movie/{movie_id}', [ReviewController::class, 'getByMovie']);
+
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout',[AuthController::class,'logout']);
+
+    // Watchlist Routes
     Route::get('/watchlist', [WatchlistController::class, 'index']);
     Route::post('/watchlist', [WatchlistController::class, 'store']);
     Route::delete('/watchlist/{id}', [WatchlistController::class, 'destroy']);
     Route::get('/watchlist/check/{movie_id}', [WatchlistController::class, 'check']);
+
+    // Review Routes
+    Route::get('/reviews', [ReviewController::class, 'index']);
+    Route::post('/reviews', [ReviewController::class, 'store']);
+    Route::get('/reviews/{movie_id}', [ReviewController::class, 'show']);
+    Route::put('/reviews/{id}', [ReviewController::class, 'update']);
+    Route::delete('/reviews/{id}', [ReviewController::class, 'destroy']);
 });
 
 
