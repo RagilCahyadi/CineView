74c988479b36428a51c31004839e4201327828b8
Rahmansyah Ragil Cahyadi <ragilccahyadi@gmail.com>
2026-01-14 23:42:22 +0700

    feat: (API) add watchlist CRUD routes and controller methods" (#29)

--- cineview-app-services/routes/api.php
+++ cineview-app-services/routes/api.php
@@ -2,12 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\WatchlistController;
 
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout',[AuthController::class,'logout']);
+    Route::get('/watchlist', [WatchlistController::class, 'index']);
+    Route::post('/watchlist', [WatchlistController::class, 'store']);
+    Route::delete('/watchlist/{id}', [WatchlistController::class, 'destroy']);
+    Route::get('/watchlist/check/{movie_id}', [WatchlistController::class, 'check']);
 });
+
 
 Route::get('/users', [AuthController::class, 'index']);
