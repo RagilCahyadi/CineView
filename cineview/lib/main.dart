05bbfd55528f16e505fe227e0ba19a7bc1c1219c
Rahmansyah Ragil Cahyadi <ragilccahyadi@gmail.com>
2026-01-18 01:23:24 +0700

    Feat: Initialize Flutter project with TMDB API integration and core dependencies

--- cineview/lib/main.dart
+++ cineview/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:cineview/presentation/providers/auth_provider.dart';
-import 'package:cineview/presentation/screen/home_page.dart';
 import 'package:cineview/presentation/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:cineview/core/theme/app_theme.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -15,9 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => AuthProvider()),
-      ],
+      providers: [ChangeNotifierProvider(create: (context) => AuthProvider())],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: AppTheme.darkTheme,
