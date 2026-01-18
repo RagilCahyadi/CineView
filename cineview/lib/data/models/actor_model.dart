7dc1422cf3770c528f467f036c8ed59e6f2c4cac
Ragil Cahyadi <ragilccahyadi@gmail.com>
2026-01-18 14:11:30 +0700

    feat: Add  actor model

---
+++ cineview/lib/data/models/actor_model.dart
@@ -0,0 +1,41 @@
+class ActorModel {
+  final int id;
+  final String name;
+  final String? profilePath;
+  final String? knownForDepartment;
+  final double popularity;
+  final int gender;
+  final List<dynamic> knownFor;
+
+  ActorModel({
+    required this.id,
+    required this.name,
+    this.profilePath,
+    this.knownForDepartment,
+    required this.popularity,
+    required this.gender,
+    required this.knownFor,
+  });
+
+  factory ActorModel.fromJson(Map<String, dynamic> json) {
+    return ActorModel(
+      id: json['id'] ?? 0,
+      name: json['name'] ?? '',
+      profilePath: json['profile_path'],
+      knownForDepartment: json['known_for_department'],
+      popularity: (json['popularity'] ?? 0).toDouble(),
+      gender: json['gender'] ?? 0,
+      knownFor: json['known_for'] ?? [],
+    );
+  }
+
+  String get genderDisplay {
+    if (gender == 1) {
+      return 'Female';
+    } else if (gender == 2) {
+      return 'Male';
+    } else {
+      return 'Unknown';
+    }
+  }
+}
